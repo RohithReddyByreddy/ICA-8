1b5e28c251839c44a6db1cd81def186d333e8584
RohithReddyByreddy <rbyreddy@asu.edu>
2022-10-27 16:50:31 -0700

    added urinal test file

---
+++ src/urinalsTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class urinalsTest {
+urinals obj=new urinals();
+
+void testCaseOne()
+{
+
+}
+
+}
